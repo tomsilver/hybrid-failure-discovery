59cf3bd19984dbf8297f36ec9e577ebaa627f55a
Tom Silver <tomssilver@gmail.com>
2025-02-02 16:02:27 -0500

    wip

--- src/hybrid_failure_discovery/failure_finders/failure_finder.py
+++ src/hybrid_failure_discovery/failure_finders/failure_finder.py
@@ -1,7 +1,6 @@
 """Base class for failure finders."""
 
 import abc
-from typing import Callable, Generic
 
 from gymnasium.core import ActType, ObsType
 
@@ -9,31 +8,7 @@ from hybrid_failure_discovery.controllers.controller import ConstraintBasedContr
 from hybrid_failure_discovery.envs.constraint_based_env_model import (
     ConstraintBasedEnvModel,
 )
-
-
-class FailureMonitor(Generic[ObsType, ActType]):
-    """Monitors a trajectory for failures."""
-
-    @abc.abstractmethod
-    def reset(self, initial_state: ObsType) -> None:
-        """Reset the monitor given a new initial state."""
-
-    @abc.abstractmethod
-    def step(self, action: ActType, state: ObsType) -> bool:
-        """Return true if failure and advance any internal state."""
-
-
-class MemorylessStateFailureMonitor(FailureMonitor[ObsType, ActType]):
-    """A failure finder that only checks a given state."""
-
-    def __init__(self, state_check: Callable[[ObsType], bool]) -> None:
-        self._state_check = state_check
-
-    def reset(self, initial_state: ObsType) -> None:
-        pass
-
-    def step(self, action: ActType, state: ObsType) -> bool:
-        return self._state_check(state)
+from hybrid_failure_discovery.failure_monitors.failure_monitor import FailureMonitor
 
 
 class FailureFinder(abc.ABC):
--- tests/failure_finders/test_random_shooting_failure_finder.py
+++ tests/failure_finders/test_random_shooting_failure_finder.py
@@ -1,16 +1,14 @@
 """Tests for random_shooting_failure_finder.py."""
 
-from tomsgeoms2d.structs import geom2ds_intersect
-
 from hybrid_failure_discovery.controllers.hovercraft_controller import (
     HoverCraftController,
 )
-from hybrid_failure_discovery.envs.hovercraft_env import HoverCraftEnv, HoverCraftState
-from hybrid_failure_discovery.failure_finders.failure_finder import (
-    MemorylessStateFailureMonitor,
-)
+from hybrid_failure_discovery.envs.hovercraft_env import HoverCraftEnv
 from hybrid_failure_discovery.failure_finders.random_shooting_failure_finder import (
     RandomShootingFailureFinder,
+)
+from hybrid_failure_discovery.failure_monitors.hovercraft_failure_monitor import (
+    HoverCraftFailureMonitor,
 )
 
 
@@ -19,16 +17,7 @@ def test_random_shooting_failure_finder():
 
     env = HoverCraftEnv()
     controller = HoverCraftController(123, env.scene_spec)
-
-    def _collision_failure(state: HoverCraftState) -> bool:
-        circ = env.get_hovercraft_circle(state)
-        for obstacle in env.scene_spec.obstacles:
-            if geom2ds_intersect(circ, obstacle):
-                return True
-        return False
-
-    failure_monitor = MemorylessStateFailureMonitor(_collision_failure)
-
+    failure_monitor = HoverCraftFailureMonitor(env.scene_spec)
     failure_finder = RandomShootingFailureFinder(seed=123)
 
     result = failure_finder.run(env, controller, failure_monitor)
