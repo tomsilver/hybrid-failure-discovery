3bc1f94f2cdb38c44fb84fcbd38d927504698c87
Tom Silver <tomssilver@gmail.com>
2025-03-22 10:44:15 -0400

    wip

--- tests/failure_finders/test_llm_commander_failure_finder.py
+++ tests/failure_finders/test_llm_commander_failure_finder.py
@@ -7,14 +7,21 @@ from typing import Any
 import pytest
 from tomsutils.llm import LargeLanguageModel, OpenAILLM
 
+from hybrid_failure_discovery.controllers.blocks_controller import BlocksController
 from hybrid_failure_discovery.controllers.hovercraft_controller import (
     HoverCraftController,
+)
+from hybrid_failure_discovery.envs.blocks_env import (
+    BlocksEnv,
 )
 from hybrid_failure_discovery.envs.hovercraft_env import (
     HoverCraftEnv,
 )
 from hybrid_failure_discovery.failure_finders.llm_commander_failure_finder import (
     LLMCommanderFailureFinder,
+)
+from hybrid_failure_discovery.failure_monitors.blocks_failure_monitor import (
+    BlocksFailureMonitor,
 )
 from hybrid_failure_discovery.failure_monitors.hovercraft_failure_monitor import (
     HoverCraftFailureMonitor,
@@ -47,7 +54,7 @@ class _MockLLM(LargeLanguageModel):
         raise NotImplementedError("TODO")
 
 
-def test_llm_commander_failure_finder() -> None:
+def test_llm_commander_failure_finder():
     """Tests for llm_commander_failure_finder.py."""
 
     mock_llm_completion = """Certainly! Here you go:
@@ -111,7 +118,7 @@ class SynthesizedCommander(Commander):
 
 
 @pytest.mark.skip(reason="Don't want to run actual LLM in CI.")
-def test_openai_llm_hovercraft_commander_failure_finder() -> None:
+def test_openai_llm_hovercraft_commander_failure_finder():
     """Run an OpenAI LLM to create a failure finder commander for
     hovercraft."""
 
@@ -129,5 +136,27 @@ def test_openai_llm_hovercraft_commander_failure_finder() -> None:
     # states = result.observations
     # imgs = [env._render_state(s) for s in states]
     # path = Path("videos") / "test-llm-commander" / "llm_commander_test.mp4"
+    # path.parent.mkdir(exist_ok=True)
+    # iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
+
+
+@pytest.mark.skip(reason="Don't want to run actual LLM in CI.")
+def test_openai_llm_blocks_commander_failure_finder():
+    """Run an OpenAI LLM to create a failure finder commander for blocks."""
+
+    llm = OpenAILLM("gpt-4o", Path("./llm_cache"), max_tokens=4096)
+
+    env = BlocksEnv()
+    controller = BlocksController(123, env.scene_spec)
+    failure_monitor = BlocksFailureMonitor()
+    failure_finder = LLMCommanderFailureFinder(llm, seed=123)
+    result = failure_finder.run(env, controller, failure_monitor)
+    assert result is not None
+
+    # Uncomment to visualize.
+    # import imageio.v2 as iio
+    # states = result.observations
+    # imgs = [env._render_state(s) for s in states]
+    # path = Path("videos") / "test-llm-commander" / "blocks_llm_commander_test.mp4"
     # path.parent.mkdir(exist_ok=True)
     # iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
