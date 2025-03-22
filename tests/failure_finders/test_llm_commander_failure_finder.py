97be5846491c5b4cf7c6fa97f451f3189b0b0744
Tom Silver <tomssilver@gmail.com>
2025-03-22 11:37:39 -0400

    wip

--- tests/failure_finders/test_llm_commander_failure_finder.py
+++ tests/failure_finders/test_llm_commander_failure_finder.py
@@ -142,7 +142,7 @@ def test_openai_llm_hovercraft_commander_failure_finder():
     # iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
 
 
-@pytest.mark.skip(reason="Don't want to run actual LLM in CI.")
+# @pytest.mark.skip(reason="Don't want to run actual LLM in CI.")
 def test_openai_llm_blocks_commander_failure_finder():
     """Run an OpenAI LLM to create a failure finder commander for blocks."""
 
@@ -151,14 +151,14 @@ def test_openai_llm_blocks_commander_failure_finder():
     env = BlocksEnv()
     controller = BlocksController(123, env.scene_spec)
     failure_monitor = BlocksFailureMonitor()
-    failure_finder = LLMCommanderFailureFinder(llm, seed=123)
+    failure_finder = LLMCommanderFailureFinder(llm, seed=123, max_trajectory_length=1000)
     result = failure_finder.run(env, controller, failure_monitor)
     assert result is not None
 
     # Uncomment to visualize.
-    # import imageio.v2 as iio
-    # states = result.observations
-    # imgs = [env._render_state(s) for s in states]
-    # path = Path("videos") / "test-llm-commander" / "blocks_llm_commander_test.mp4"
-    # path.parent.mkdir(exist_ok=True)
-    # iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
+    import imageio.v2 as iio
+    states = result.observations
+    imgs = [env._render_state(s) for s in states]
+    path = Path("videos") / "test-llm-commander" / "blocks_llm_commander_test.mp4"
+    path.parent.mkdir(exist_ok=True)
+    iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
