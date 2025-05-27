e3503d68772c701dd37a1166063e10dc19c030a5
Apurva <apurva.b@princeton.edu>
2025-05-27 10:25:15 -0400

    finding_initial_states

--- tests/failure_finders/test_llm_commander_failure_finder.py
+++ tests/failure_finders/test_llm_commander_failure_finder.py
@@ -117,7 +117,7 @@ class SynthesizedCommander(Commander):
     # iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
 
 
-# @pytest.mark.skip(reason="Don't want to run actual LLM in CI.")
+@pytest.mark.skip(reason="Don't want to run actual LLM in CI.")
 def test_openai_llm_hovercraft_commander_failure_finder():
     """Run an OpenAI LLM to create a failure finder commander for
     hovercraft."""
@@ -142,7 +142,7 @@ def test_openai_llm_hovercraft_commander_failure_finder():
     # iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
 
 
-# @pytest.mark.skip(reason="Don't want to run actual LLM in CI.")
+@pytest.mark.skip(reason="Don't want to run actual LLM in CI.")
 def test_openai_llm_blocks_commander_failure_finder():
     """Run an OpenAI LLM to create a failure finder commander for blocks."""
 
@@ -158,10 +158,9 @@ def test_openai_llm_blocks_commander_failure_finder():
     assert result is not None
 
     # Uncomment to visualize.
-    import imageio.v2 as iio
-
-    states = result.observations
-    imgs = [env._render_state(s) for s in states]
-    path = Path("videos") / "test-llm-commander" / "blocks_llm_commander_test.mp4"
-    path.parent.mkdir(exist_ok=True)
-    iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
+    # import imageio.v2 as iio
+    # states = result.observations
+    # imgs = [env._render_state(s) for s in states]
+    # path = Path("videos") / "test-llm-commander" / "blocks_llm_commander_test.mp4"
+    # path.parent.mkdir(exist_ok=True)
+    # iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
