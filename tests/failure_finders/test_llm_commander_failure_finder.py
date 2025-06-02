99c78e3d3c0cd7192706aeea096c55ae9003253f
Tom Silver <tomssilver@gmail.com>
2025-06-02 11:31:40 -0400

    remove pybullet dependency

--- tests/failure_finders/test_llm_commander_failure_finder.py
+++ tests/failure_finders/test_llm_commander_failure_finder.py
@@ -7,21 +7,14 @@ from typing import Any
 import pytest
 from tomsutils.llm import LargeLanguageModel, OpenAILLM
 
-from hybrid_failure_discovery.controllers.blocks_controller import BlocksController
 from hybrid_failure_discovery.controllers.hovercraft_controller import (
     HoverCraftController,
-)
-from hybrid_failure_discovery.envs.blocks_env import (
-    BlocksEnv,
 )
 from hybrid_failure_discovery.envs.hovercraft_env import (
     HoverCraftEnv,
 )
 from hybrid_failure_discovery.failure_finders.llm_commander_failure_finder import (
     LLMCommanderFailureFinder,
-)
-from hybrid_failure_discovery.failure_monitors.blocks_failure_monitor import (
-    BlocksFailureMonitor,
 )
 from hybrid_failure_discovery.failure_monitors.hovercraft_failure_monitor import (
     HoverCraftFailureMonitor,
@@ -138,29 +131,5 @@ def test_openai_llm_hovercraft_commander_failure_finder():
     # states = result.observations
     # imgs = [env._render_state(s) for s in states]
     # path = Path("videos") / "test-llm-commander" / "hovercraft_llm_commander_test.mp4"
-    # path.parent.mkdir(exist_ok=True)
-    # iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
-
-
-@pytest.mark.skip(reason="Don't want to run actual LLM in CI.")
-def test_openai_llm_blocks_commander_failure_finder():
-    """Run an OpenAI LLM to create a failure finder commander for blocks."""
-
-    llm = OpenAILLM("gpt-4o", Path("./llm_cache"), max_tokens=4096)
-
-    env = BlocksEnv()
-    controller = BlocksController(123, env.scene_spec)
-    failure_monitor = BlocksFailureMonitor()
-    failure_finder = LLMCommanderFailureFinder(
-        llm, seed=123, max_trajectory_length=1000
-    )
-    result = failure_finder.run(env, controller, failure_monitor)
-    assert result is not None
-
-    # Uncomment to visualize.
-    # import imageio.v2 as iio
-    # states = result.observations
-    # imgs = [env._render_state(s) for s in states]
-    # path = Path("videos") / "test-llm-commander" / "blocks_llm_commander_test.mp4"
     # path.parent.mkdir(exist_ok=True)
     # iio.mimsave(path, imgs, fps=env.metadata["render_fps"])
