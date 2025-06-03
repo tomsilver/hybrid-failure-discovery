12d7147642c3ee6d0b8e81756649ae550f85fc1f
Apurva Badithela <13437374+abadithela@users.noreply.github.com>
2025-06-03 09:34:23 -0400

    Base class for sampling initial state (#20)

---
+++ src/hybrid_failure_discovery/commander/initial_state_commander.py
@@ -0,0 +1,14 @@
+"""Base class for a commander."""
+
+import abc
+from typing import Generic
+
+from gymnasium.core import ObsType
+
+
+class InitialStateCommander(Generic[ObsType]):
+    """Base class for setting the initial state."""
+
+    @abc.abstractmethod
+    def initialize(self) -> ObsType:
+        """Return the initial state to start the trajectory from."""
